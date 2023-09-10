4ca45fea2e6ec5bcc983922281c2651ddcaa9069
Kevin Quail <kevinq.7365@gmail.com>
2023-09-10 09:49:17 -0500

    added POST to json database*add to favorite click function created*add favorite function working propely and making post of object*

---
+++ src/services/CryptoCurrencyPost.js
@@ -0,0 +1,9 @@
+export const cryptoCurrencyPost = (item) => {
+  return fetch("http://localhost:8088/favorites", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(item),
+  });
+};
