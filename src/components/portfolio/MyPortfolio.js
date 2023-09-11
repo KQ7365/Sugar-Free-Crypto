bba50cd5c1915fa4efd232cd86b19a02432b1dbb
Kevin Quail <kevinq.7365@gmail.com>
2023-09-10 19:21:45 -0500

    created json fetch module*favorite list in my portfolio rendered and fetching favorited crypto

--- src/components/portfolio/MyPortfolio.js
+++ src/components/portfolio/MyPortfolio.js
@@ -1,1 +1,26 @@
-export const MyPortfolio = () => {};
+import { useEffect, useState } from "react";
+import { CryptoFavoriteList } from "../../services/CryptoFavoriteList";
+
+export const MyPortfolio = () => {
+  const [favoriteCryptoList, setFavoriteCryptoList] = useState([]);
+
+  useEffect(() => {
+    CryptoFavoriteList().then((favoriteArray) => {
+      setFavoriteCryptoList(favoriteArray);
+    });
+  }, []);
+  return (
+    <form>
+      <fieldset>
+        <select>
+          <option value="" disabled selected hidden>
+            Choose an option
+          </option>
+          {favoriteCryptoList.map((favObj) => {
+            return <option value={favObj.id}>{favObj.name}</option>;
+          })}
+        </select>
+      </fieldset>
+    </form>
+  );
+};
