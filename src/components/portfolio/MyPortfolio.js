37f1b64fcf44b3740a595918a929a4a01a2663a5
Kevin Quail <kevinq.7365@gmail.com>
2023-09-12 20:28:29 -0500

    state is now being captured on form select

--- src/components/portfolio/MyPortfolio.js
+++ src/components/portfolio/MyPortfolio.js
@@ -3,21 +3,31 @@ import { CryptoFavoriteList } from "../../services/CryptoFavoriteList";
 
 export const MyPortfolio = () => {
   const [favoriteCryptoList, setFavoriteCryptoList] = useState([]);
+  const [cryptoNameSelect, setCryptoNameSelect] = useState([]);
 
   useEffect(() => {
     CryptoFavoriteList().then((favoriteArray) => {
       setFavoriteCryptoList(favoriteArray);
     });
   }, []);
+
+  const handleSelectChange = (event) => {
+    const cryptoNameCopy = { ...cryptoNameSelect };
+    cryptoNameCopy[event.target.name] = event.target.value;
+    setCryptoNameSelect(cryptoNameCopy);
+  };
+
   return (
     <form>
       <fieldset>
-        <select>
-          <option value="" disabled selected hidden>
-            Choose an option
-          </option>
+        <select onChange={handleSelectChange}>
+          <option>Choose an option</option>
           {favoriteCryptoList.map((favObj) => {
-            return <option value={favObj.id}>{favObj.name}</option>;
+            return (
+              <option key={favObj.id} value={favObj.name}>
+                {favObj.name}
+              </option>
+            );
           })}
         </select>
       </fieldset>
