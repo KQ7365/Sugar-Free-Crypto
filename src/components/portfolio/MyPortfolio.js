8e146d50ee6c3344fc04dfbdaef7f3bdd1e05605
Kevin Quail <kevinq.7365@gmail.com>
2023-09-16 12:39:29 -0500

    add notes POST working as well as displaying on browser.Logic set to only show current users notes.changed image url to a resource url instead

--- src/components/portfolio/MyPortfolio.js
+++ src/components/portfolio/MyPortfolio.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { CryptoFavoriteList } from "../../services/CryptoFavoriteList";
 import { NotesPost } from "../../services/NotesPost";
+import "./MyPortfolio.css";
+import { favoriteResourceService } from "../../services/favoriteService";
 
 export const MyPortfolio = ({ currentUser }) => {
   const [favoriteCryptoList, setFavoriteCryptoList] = useState([]);
+  const [cryptoNotesItem, setCryptoNotesItem] = useState([]);
   const [newCryptoObject, setNewCryptoObject] = useState({
     cryptoName: "",
     note: "",
-    image: "",
+    resourceUrl: "",
     currentUserId: { currentUser },
   });
 
@@ -19,6 +22,15 @@ export const MyPortfolio = ({ currentUser }) => {
       setFavoriteCryptoList(filteredFavorites);
     });
   }, [currentUser]);
+
+  useEffect(() => {
+    favoriteResourceService().then((cryptoNotes) => {
+      const filteredNotes = cryptoNotes.filter(
+        (note) => note.currentUser.currentUser.id === currentUser.id
+      );
+      setCryptoNotesItem(filteredNotes);
+    });
+  }, [newCryptoObject, currentUser]);
 
   const handleInputChange = (e) => {
     const itemCopy = { ...newCryptoObject };
@@ -32,76 +44,101 @@ export const MyPortfolio = ({ currentUser }) => {
     const newNotesItem = {
       name: newCryptoObject.cryptoName,
       note: newCryptoObject.note,
-      imageUrl: newCryptoObject.image,
+      resource: newCryptoObject.resourceUrl,
       currentUser: { currentUser },
     };
 
-    NotesPost(newNotesItem);
+    NotesPost(newNotesItem).then(() => {
+      setNewCryptoObject({
+        cryptoName: "",
+        note: "",
+        resourceUrl: "",
+        currentUserId: { currentUser },
+      });
+    });
   };
 
   return (
     <div>
-      <body>
-        <div>
-          <h1>Favorite Resources</h1>
+      <body className="favoriteCardParentDiv">
+        <div className="favoriteCard">
+          <h1>Favorite Links</h1>
         </div>
-        <div>
-          <h1>Favorite Cryptocurrencies</h1>
+        <div className="favoriteCard">
+          <h1>Favorite Crypto</h1>
           {favoriteCryptoList.map((favObj) => {
             return (
               <div key={favObj.id} value={favObj.cryptoName}>
-                {favObj.cryptoName} {favObj.price}
+                Crypto: {favObj.cryptoName} | Price: {favObj.price}
               </div>
             );
           })}
         </div>
       </body>
 
-      <form>
-        <fieldset>
-          <select
-            onChange={handleInputChange}
-            name="cryptoName"
-            value={favoriteCryptoList.id}
-          >
-            <option value="0">Choose one of your crypto favorites</option>
-            {favoriteCryptoList.map((favObj) => {
-              return (
-                <option key={favObj.id} value={favObj.cryptoName}>
-                  {favObj.cryptoName}
-                </option>
-              );
-            })}
-          </select>
-        </fieldset>
+      <div className="wholeEntry">
+        <form>
+          <fieldset>
+            <select
+              onChange={handleInputChange}
+              name="cryptoName"
+              value={newCryptoObject.cryptoName}
+            >
+              <option value="0">Choose one of your crypto favorites</option>
+              {favoriteCryptoList.map((favObj) => {
+                return (
+                  <option key={favObj.id} value={favObj.cryptoName}>
+                    {favObj.cryptoName}
+                  </option>
+                );
+              })}
+            </select>
+          </fieldset>
 
-        <fieldset>
-          <label>Enter Notes</label>
-          <textarea
-            name="note"
-            rows={6}
-            required
-            type="text"
-            className="setLater"
-            placeholder=""
-            onChange={handleInputChange}
-          ></textarea>
-        </fieldset>
+          <fieldset>
+            <label>Enter Notes</label>
+            <textarea
+              name="note"
+              rows={6}
+              required
+              type="text"
+              className="setLater"
+              placeholder=""
+              onChange={handleInputChange}
+              value={newCryptoObject.note}
+            ></textarea>
+          </fieldset>
 
-        <fieldset>
-          <label>Enter image link</label>
-          <input
-            name="image"
-            required
-            type="text"
-            className="setLater"
-            placeholder="https://www.example.com"
-            onChange={handleInputChange}
-          ></input>
-        </fieldset>
+          <fieldset>
+            <label>Enter Resource Url</label>
+            <input
+              value={newCryptoObject.resourceUrl}
+              name="resourceUrl"
+              required
+              type="text"
+              className="setLater"
+              placeholder="https://www.example.com"
+              onChange={handleInputChange}
+            ></input>
+          </fieldset>
 
-        <button onClick={handleAddNotesClick}>Add Note</button>
-      </form>
+          <button onClick={handleAddNotesClick}>Add Note</button>
+        </form>
+      </div>
+      <div>
+        <h2>Crypto Notes</h2>
+      </div>
+      <div>
+        {cryptoNotesItem.map((noteObj) => {
+          return (
+            <div key={noteObj.id} value={noteObj.id}>
+              Crypto: {noteObj.name}
+              Notes: {noteObj.note}
+              Resource Url: {noteObj.resource}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
