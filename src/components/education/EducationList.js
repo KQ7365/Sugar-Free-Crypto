0cc45ce47816cf8d5f52e96c04f8bb552f1a3323
Kevin Quail <kevinq.7365@gmail.com>
2023-09-18 11:06:04 -0500

    finished css and MVP on favorites page. Now moving onto final page dev of Resources

--- src/components/education/EducationList.js
+++ src/components/education/EducationList.js
@@ -1,1 +1,12 @@
-export const EducationList = () => {};
+import "./Education.css";
+
+export const EducationList = () => {
+  return (
+    <div>
+      <div className="header-container">
+        <h1 className="headText">Resources </h1>
+        <img className="mainIMG" alt="cyber img" src="resource.png" />
+      </div>
+    </div>
+  );
+};
